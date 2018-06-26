4603715fbf3dcaaa132272ecfdedf44bb28cdaf4
Forrest Benton <elliottbenton@gmail.com>
2018-06-26 19:40:35 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,4 +41,12 @@ class TicTacToe
     end
   end
   
+  def valid_move?(index)
+    if !(position_taken?(index)) && index.between?(0,8)
+      return true
+    else
+      return false
+    end
+  end
+  
 end
