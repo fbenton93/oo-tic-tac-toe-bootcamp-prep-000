92ba47be98ab3c2ca6334b23f1e75c9325f90efa
Forrest Benton <elliottbenton@gmail.com>
2018-06-26 20:04:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,15 +50,20 @@ class TicTacToe
   end
   
   def turn
-    puts "Please enter 1-9:"
-    input = gets.strip
-    index = input_to_index(input)
-    if valid_move?(index)
-      move(index, current_player)
-      display_board
-    else
-      puts "Invalid response. Try again."
-      turn
+  
+    if over? != true
+      puts "Please enter 1-9:"
+      input = gets.strip
+      index = input_to_index(input)
+
+    
+      if valid_move?(index)
+        move(index,current_player)
+        display_board
+        return @board
+      else
+        return turn
+      end
     end
   end
   
